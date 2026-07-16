7137e65815718658ce0b77a8a7af7639b06fb9cd
Shiv Sharan Tripathi <tripathishivsharan898@gmail.com>
2026-07-16 20:50:37 +0530

    area of square

---
+++ areaofsquare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float side,area;
+    printf("Enter the side of the square: ");
+    scanf("%f", &side);
+    area = side * side;
+    printf("Area of the square: %.2f\n", area);
+    return 0;
+}
