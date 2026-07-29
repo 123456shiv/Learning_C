ef7a1b18b68d311d604164af115bc6796145fbb7
Shiv Sharan Tripathi <tripathishivsharan898@gmail.com>
2026-07-29 22:22:32 +0530

    divide by 2 and 3

---
+++ divide by 2 and 3.c
@@ -0,0 +1,17 @@
+#include<Stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+
+    if(a%2==0 && a%3==0)
+    {
+        printf("%d is divisible by 2 and 3",a);
+    }
+    else
+    {
+        printf("%d is not divisible by 2 and 3",a);
+    }
+}
